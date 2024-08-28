79509c168c4f9aa037fb5c2d23aa490a47d33260
Tanmesh Chandra <Tanmesh.Chandra@godigit.com>
2024-08-28 18:36:08 +0530

    some changes

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpService } from '../http/http.service';
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  httpService: any;
+
+  header = {
+    'x-access-token': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjdhMDkzY2VhZTVjNDAwMGVkMGVkMDIiLCJpYXQiOjE3MjQ3NjI1OTAsImV4cCI6MTcyNDg0ODk5MH0.2aDnHhNdkpkt5woXZLYR5Pd_9yx4WRdOIb_M7x4vs0M`,
+  };
 
   constructor() { }
+ 
+  getAllCartApiCall() {
+    return this.httpService.GetApiCall('bookstore_user/get_cart_items',{headers:this.header});
+  }
 }
