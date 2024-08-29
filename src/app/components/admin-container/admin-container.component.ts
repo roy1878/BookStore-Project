668b8047c265886f22c6093e89028e5fc9406f45
priyakri104 <priya.kumari@godigit.com>
2024-08-30 00:37:17 +0530

    functionality added

--- src/app/components/admin-container/admin-container.component.ts
+++ src/app/components/admin-container/admin-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-container',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminContainerComponent implements OnInit {
 
+ 
   constructor() { }
-
+  
   ngOnInit(): void {
+    
+
+    
+ 
+ 
   }
 
 }
