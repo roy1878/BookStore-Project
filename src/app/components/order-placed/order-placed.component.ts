749b317d7e2cd608f5d58d11a5354c922a38562e
Tanmesh Chandra <Tanmesh.Chandra@godigit.com>
2024-08-30 11:53:24 +0530

    Conflict Resolved

--- src/app/components/order-placed/order-placed.component.ts
+++ src/app/components/order-placed/order-placed.component.ts
@@ -1,30 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CartService } from 'src/app/services/cart/cart.service';
-import { DataService } from 'src/app/services/data/data.service';
+
 @Component({
   selector: 'app-order-placed',
   templateUrl: './order-placed.component.html',
   styleUrls: ['./order-placed.component.scss']
 })
 export class OrderPlacedComponent implements OnInit {
-  cartItems: any[] = [];
-  constructor(private router: Router,private cartService: CartService,private dataService: DataService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.dataService.currentCartList.subscribe({
-      next:(res:any)=>{
-        this.cartItems=res;
-        console.log("cartlist",res);
-        console.log(this.cartItems);
-  
-  
-      }
-     })
   }
-
-continueShopping(){
-  this.router.navigate(['dashboard/books']);
-}
 
 }
