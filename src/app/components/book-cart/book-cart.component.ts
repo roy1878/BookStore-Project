d85681f5a63337c30d1006d3539789f2e485c55c
Tanmesh Chandra <Tanmesh.Chandra@godigit.com>
2024-08-30 15:37:04 +0530

    All cart features Working

--- src/app/components/book-cart/book-cart.component.ts
+++ src/app/components/book-cart/book-cart.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { LoginSignupComponent } from '../login-signup/login-signup.component';
 import { CartService } from 'src/app/services/cart/cart.service';
 import { DataService } from 'src/app/services/data/data.service';
+import { UserService } from 'src/app/services/user/user.service';
 @Component({
   selector: 'app-book-cart',
   templateUrl: './book-cart.component.html',
@@ -23,10 +24,24 @@ export class BookCartComponent implements OnInit {
   cartItemId: string = 'cartlist._id';
   cartItems: any[] = [];
 
+  PDisDisabled:boolean=true;
+  CDisDisabled:boolean=false;
+  customerAddress:string="";
+  customerCity:string="";
+  customerState:string="";
+  customerAddType:any="Customer Address";
+  customerDetails:any;
+  showFirstDiv: boolean = true;
+  customerAddreessList:any[]=[];
+
+  editCard:boolean=false;
+  displaySpan:boolean=false;
+
   constructor(
     private cartService: CartService,
     private dataService: DataService,
-    private route: Router
+    private route: Router,
+    private userService:UserService
   ) {}
   hideBtn1() {
     this.isBtnVisible = false;
@@ -63,8 +78,129 @@ export class BookCartComponent implements OnInit {
         console.log(this.cartItems);
       },
     });
+    this.dataService.currentCartList.subscribe({
+      next:(res:any)=>{
+        console.log(res);
+        this.customerDetails=res[0].user_id.address;
+       
+        this.customerDetails = this.customerDetails.map((addressData: any) => ({
+          ...addressData,
+          showFirstDiv: true
+        }));
+        console.log("cust",this.customerDetails);
+        
+
+        
+
+        
+        // this.dataService.updateCustomerAddressList( this.customerDetails);
+        console.log("add",res[0].user_id.address);
+      }
+    })
+  }
+  
+  PDenableEditing(): void {
+    this.PDisDisabled=false;
+  }
+
+  CDenableEditing(): void {
+    this.CDisDisabled=false;
     
   }
+
+  shouldShowFirstDiv(addressData: any): boolean {
+    return addressData.showFirstDiv;
+  }
+
+
+  toggleDivs(addressData:any) {
+    // this.showFirstDiv = !this.showFirstDiv;
+    addressData.showFirstDiv = !addressData.showFirstDiv;
+    const action=addressData.addressType;
+    console.log(action);
+
+    if (action == 'Office') {
+      const officeItems = this.customerDetails.find((item: any) => {
+        return item.addressType === 'Office';
+      });
+
+      console.log(officeItems);
+
+      this.customerAddress = officeItems.fullAddress;
+      this.customerCity = officeItems.city;
+      this.customerState = officeItems.state;
+      this.customerAddType = officeItems.addressType;
+      console.log(officeItems.fullAddress);
+    }
+
+    else if (action == 'Home') {
+      const officeItems = this.customerDetails.find((item: any) => {
+        return item.addressType === 'Home';
+      });
+
+      console.log(officeItems);
+
+      this.customerAddress = officeItems.fullAddress;
+      this.customerCity = officeItems.city;
+      this.customerState = officeItems.state;
+      this.customerAddType = officeItems.addressType;
+      console.log(officeItems.fullAddress);
+    }
+   else if (action == 'Other') {
+      const officeItems = this.customerDetails.find((item: any) => {
+        return item.addressType === 'Other';
+      });
+      this.customerAddress = officeItems.fullAddress;
+      this.customerCity = officeItems.city;
+      this.customerState = officeItems.state;
+      this.customerAddType = officeItems.addressType;
+      console.log(officeItems.fullAddress);
+    }
+
+
+
+
+    
+  }
+
+  
+
+  saveAddress(){
+   
+
+    console.log(this.customerCity);
+    console.log(this.customerAddType);
+    this.customerDetails={
+      "addressType": this.customerAddType,
+      "fullAddress": this.customerAddress,
+      "city": this.customerCity,
+      "state": this.customerState
+    }
+
+    // this.CDisDisabled = true;
+    this.userService.updateCustomerDetails(this.customerDetails).subscribe({
+      next:(res:any)=>{
+        console.log("Customer address update res ", res);
+        
+      }
+    })
+    this.showFirstDiv = !this.showFirstDiv;
+    alert("Address Updated Successfully");
+    window.location.reload();
+
+  }
+
+  add_new_add(){
+        this.showFirstDiv = !this.showFirstDiv;
+
+
+  }
+
+
+    
+
+   
+  
 
   count: number = 1;
 
